ca573157fb65d275680402f8b0a5d69172790cb0
vitaliy <vitaliy@backbase.com>
2020-02-05 00:03:59 +0200

    boilerplate

---
+++ README.md
@@ -0,0 +1,17 @@
+# Server side ReactJS app (boilerplate)
+
+Install
+---
+
+`npm install`
+
+`npm run build`
+
+
+
+Usage
+---
+
+`npm start`
+
+and run in browser `http://localhost:3000`
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] }
+    ]
+  }
+};
