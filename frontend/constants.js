e36367144218147ce7066a621ca38a8ffa614371
vitaliy <vitaliy@backbase.com>
2020-02-06 22:55:29 +0200

    modify checkbox label onClick

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,4 +1,5 @@
 export const getAppsUrl = 'http://localhost:3000/api/apps';
 export const rootElementId = 'root';
 export const triggerElementId = 'trigger';
+export const triggerLabelElementId = 'trigger-label';
 export const events = ['show-list', 'show-card'];
--- frontend/views/trigger.js
+++ frontend/views/trigger.js
@@ -1,12 +1,14 @@
-import { triggerElementId, events } from '../constants';
+import { triggerElementId, triggerLabelElementId, events } from '../constants';
 
 class ListTrigger {
   constructor(bus) {
     const [listEvent, cardEvent] = events;
     const triggerEl = document.querySelector(`#${triggerElementId}`);
+    const triggerLabelEl = document.querySelector(`#${triggerLabelElementId}`);
     triggerEl.addEventListener('click', (e) => {
       const isListView = e.target.checked;
       isListView ? bus.publish(listEvent, []) : bus.publish(cardEvent, []);
+      isListView ? triggerLabelEl.textContent = 'Show as an awesome grid' : triggerLabelEl.textContent = 'Show as list';
     });
   }
 }
